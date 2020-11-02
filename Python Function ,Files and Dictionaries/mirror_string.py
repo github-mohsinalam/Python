a4cbd93407628cdf726426aa479a1f37f49dc9a3
Mohsin Alam <mohsinalam3851@gmail.com>
2020-11-02 10:10:49 +0530

    Create mirror_string.py
    Exercise 12.16
    Question 4

---
+++ Python Function ,Files and Dictionaries/mirror_string.py
@@ -0,0 +1,12 @@
+#Write a function that mirrors its string argument, generating a string containing the original string and the string backwards.
+
+
+def reverse(astring):
+    astring_rev = ''
+    for char in astring :
+        astring_rev = char + astring_rev
+    return astring_rev
+def mirror(mystr):
+    return mystr+reverse(mystr)
+print(mirror('good'))
+    
