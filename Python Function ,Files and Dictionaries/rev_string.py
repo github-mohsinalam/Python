f3f4a5274e9f9bee8d2d21740a65226ef5b07c02
Mohsin Alam <mohsinalam3851@gmail.com>
2020-11-02 10:08:41 +0530

    Create rev_string.py
    Exercise 12.16
    question 3

---
+++ Python Function ,Files and Dictionaries/rev_string.py
@@ -0,0 +1,9 @@
+#Write a function that reverses its string argument.
+
+def reverse(astring):
+    astring_rev = ''
+    for char in astring :
+        astring_rev = char + astring_rev
+    return astring_rev
+print(reverse('xyz'))
+        
