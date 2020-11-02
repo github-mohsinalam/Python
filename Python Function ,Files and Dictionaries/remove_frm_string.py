f5295b709708b9f1d341b80503943355977bfb36
Mohsin Alam <mohsinalam3851@gmail.com>
2020-11-02 10:12:34 +0530

    Create remove_frm_string.py
    Exercise 12.16
    Question 5

---
+++ Python Function ,Files and Dictionaries/remove_frm_string.py
@@ -0,0 +1,12 @@
+#Write a function that removes all occurrences of a given letter from a string.
+
+
+def remove_letter(theLetter, theString):
+    lst_theString = list(theString)
+    for i in range(lst_theString.count(theLetter)):
+        lst_theString.remove(theLetter)
+    newString = ''.join(lst_theString)
+    return newString
+print('abahca')
+    
+
