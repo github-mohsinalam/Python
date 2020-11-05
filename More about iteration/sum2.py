30235977dc7c99251e7575e2ca9efe9b9aa28157
Mohsin Alam <mohsinalam3851@gmail.com>
2020-11-05 11:52:35 +0530

    CA_14.8_Que_5

---
+++ More about iteration/sum2.py
@@ -0,0 +1,18 @@
+#Below is a for loop that works. Underneath the for loop, rewrite the problem so that it does the same thing, but using a while loop instead of a for loop. 
+#Assign the accumulated total in the while loop code to the variable sum2. Once complete, sum2 should equal sum1.
+
+
+
+sum1 = 0
+
+lst = [65, 78, 21, 33]
+
+for x in lst:
+    sum1 = sum1 + x
+sum2 = 0
+i = 0
+while i < len(lst):
+    sum2 = sum2 + lst[i]
+    i = i+1
+
+
