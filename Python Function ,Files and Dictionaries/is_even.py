04c84cabddc17abf8221178525f9ed2a3fcf66b4
Mohsin Alam <mohsinalam3851@gmail.com>
2020-11-05 10:42:40 +0530

    Ex_12.16_Que_14

--- Python Function ,Files and Dictionaries/is_even.py
+++ Python Function ,Files and Dictionaries/is_even.py
@@ -1,4 +1,5 @@
-#Write a function called is_even(n) that takes an integer as an argument and returns True if the argument is an even number and False if it is odd.
+#Write a function called is_even(n) that takes an integer as an argument and
+#returns True if the argument is an even number and False if it is odd.
 
 def is_even(n):
     if n%2 == 0:
