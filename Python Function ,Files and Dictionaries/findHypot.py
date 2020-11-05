24664a7a41c09a8fe2e83bdf24ed4ea5140b02d7
Mohsin Alam <mohsinalam3851@gmail.com>
2020-11-05 10:40:26 +0530

    Exe12.16_Que_13

--- Python Function ,Files and Dictionaries/findHypot.py
+++ Python Function ,Files and Dictionaries/findHypot.py
@@ -1,6 +1,6 @@
 #Write a function findHypot.
 #The function will be given the length of two sides of a right-angled triangle and it should return the length of the hypotenuse. 
-#(Hint: x ** 0.5 will return the square root, or use sqrt from the math module)
+#(Hint: x ** 0.5 will return the square root, or use sqrt from the math module).
 
 
 def findHypot(a,b):
