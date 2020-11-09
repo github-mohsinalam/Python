ae62c0c975774c09e705285e9f04c0823551e3eb
Mohsin Alam <mohsinalam3851@gmail.com>
2020-11-09 12:51:10 +0530

    CA_16.10_que_10

---
+++ Sorting/last_four_lambda.py
@@ -0,0 +1,7 @@
+#Sort the list ids by the last four digits of each id.
+#Do this using lambda and not using a defined function. Save this sorted list in the variable sorted_id.
+
+ids = [17573005, 17572342, 17579000, 17570002, 17572345, 17579329]
+
+
+sorted_id = sorted(ids, key = lambda id : str(id)[-4:])
