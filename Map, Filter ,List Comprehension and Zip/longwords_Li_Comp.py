7ebf63360ffb9117818f40678bbc4be06dbef5dd
Mohsin Alam <mohsinalam3851@gmail.com>
2020-11-22 10:33:47 +0530

    Update longwords_Li_Comp.py
    Ex_23.6
    Que_10

--- Map, Filter ,List Comprehension and Zip/longwords_Li_Comp.py
+++ Map, Filter ,List Comprehension and Zip/longwords_Li_Comp.py
@@ -1,5 +1,6 @@
 #Define longewords using filter
 
+
 def longwords_Li_Comp(strings):
     """Return a shorter list of strings containing only the strings with more than four characters. Use a list comprehension."""
     return [string for string in strings if len(string)>4 ]
