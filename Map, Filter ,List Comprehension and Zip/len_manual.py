315e6b0fb4ad594bea5864fae25657c1fe2e7304
Mohsin Alam <mohsinalam3851@gmail.com>
2020-11-22 09:01:07 +0530

    Create len_manual.py
    Ex_23.6
    Que_2

---
+++ Map, Filter ,List Comprehension and Zip/len_manual.py
@@ -0,0 +1,12 @@
+#Use manual accumulation to define the lengths function below.
+
+def lengths(strings):
+    """lengths takes a list of strings as input and returns a list of numbers that are the lengths
+    of strings in the input list. Use manual accumulation!"""
+    # fill in this function's definition to make the test pass.
+    lst_str_len = []
+    for string in strings:
+        lst_strlen.append(len(string))
+    return lst_str_len
+
+
