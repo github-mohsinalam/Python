f7f4f395c96368ee0f6d490f4769c76784c4b14e
Mohsin Alam <mohsinalam3851@gmail.com>
2020-11-26 15:29:49 +0530

    Create num.py
    CA_10.15 (Chapter Assessment)
    Que_1

---
+++ Files/num.py
@@ -0,0 +1,7 @@
+#The textfile, travel_plans.txt, contains the summer travel plans for someone with some commentary.
+#Find the total number of characters in the file and save to the variable num.
+
+with open("travel_plans.txt") as travel_plans:
+    num = len(travel_plans.read())
+print(num)
+
